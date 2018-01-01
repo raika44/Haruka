9126200ea025c754bf5fa125cec2c51b704bb69b
raika44 <33775480+raika44@users.noreply.github.com>
2018-01-01 17:02:26 +0700

    Update iya.py

--- iya.py
+++ iya.py
@@ -788,22 +788,22 @@ def bot(op):
         if op.type == 19:
 		if wait["AutoKick"] == True:
 		    try:
-			if op.param3 in Creator:
+			if op.param3 in creator:
 			 if op.param3 in admin:
 			  if op.param3 in Bots:
 			   if op.param3 in staff:				
 			      pass
-		         if op.param2 in Creator:
-		          if op.param2 in admin:
-		           if op.param2 in Bots:
-			    if op.param2 in staff:		
-		                pass
-		            else:
+		        if op.param2 in creator:
+		         if op.param2 in admin:
+		          if op.param2 in Bots:
+			   if op.param2 in staff:		
+		               pass
+		           else:
 		                random.choice(KAC).kickoutFromGroup(op.param1,[op.param2])
 		                if op.param2 in wait["blacklist"]:
 		                    pass
-		        else:
-			    random.choice(KAC).inviteIntoGroup(op.param1,[op.param3])
+		                else:
+			            random.choice(KAC).inviteIntoGroup(op.param1,[op.param3])
 		    except:
 		        try:
 			    if op.param2 not in creator:
@@ -828,8 +828,6 @@ def bot(op):
 			                    pass
 			                else:
                                             wait["blacklist"][op.param2] = True
-                                            f=codecs.open('st2__b.json','w','utf-8')
-                                            json.dump(wait["blacklist"], f, sort_keys=True, indent=4,ensure_ascii=False)
 		    if op.param2 in wait["blacklist"]:
                         pass
                     else:
@@ -840,8 +838,6 @@ def bot(op):
 			                pass
 		                    else:
                                         wait["blacklist"][op.param2] = True
-                                        f=codecs.open('st2__b.json','w','utf-8')
-                                        json.dump(wait["blacklist"], f, sort_keys=True, indent=4,ensure_ascii=False)
 		else:
 		    pass	
 	
