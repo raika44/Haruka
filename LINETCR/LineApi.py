a79bf3939691143b1138075fa63f7387c024e8b7
raika44 <33775480+raika44@users.noreply.github.com>
2018-01-02 19:29:41 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -48,14 +48,14 @@ class LINE:
    }
 
     self.Poll = Poll(self.authToken)
-    #self.channel = channel.Channel(self.authToken)
-    #self.channel.login()
+    self.channel = channel.Channel(self.authToken)
+    self.channel.login()
 	
-    #self.mid = self.channel.mid
-    #self.channel_access_token = self.channel.channel_access_token
-    #self.token = self.channel.token
-    #self.obs_token = self.channel.obs_token
-    #self.refresh_token = self.channel.refresh_token
+    self.mid = self.channel.mid
+    self.channel_access_token = self.channel.channel_access_token
+    self.token = self.channel.token
+    self.obs_token = self.channel.obs_token
+    self.refresh_token = self.channel.refresh_token
 
 
   """User"""
