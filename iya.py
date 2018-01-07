c6e2dcf489ae9423bc4e24064d36929efd70288b
raika44 <33775480+raika44@users.noreply.github.com>
2018-01-07 18:10:32 +0700

    Update iya.py

--- iya.py
+++ iya.py
@@ -1177,34 +1177,30 @@ def bot(op):
 	  except:
 	         pass
         if op.type == 19:
-		if wait["protect"] == True:
+		if wait["AutoKick"][op.param1] == True:
 		    try:
-			if op.param3 in Bots:
-			    pass
-			elif op.param3 in creator:
-			      pass
+			if op.param3 in Creator:
 			elif op.param3 in admin:
-			      pass
-			elif op.param3 in staff:
-			      pass
-		        if op.param2 in Bots:
+			elif op.param3 in Bots:
+			elif op.param3 in staff:				
 			    pass
-		        elif op.param2 in creator:
-			      pass
-		        elif op.param2 in admin:
-			      pass
-		        elif op.param2 in staff:
-			      pass
+		         if op.param2 in Creator:
+		         elif op.param2 in admin:
+		         elif op.param2 in Bots:
+		         elif op.param2 in staff:				
+		             pass
+		         else:
+		             random.choice(KAC).kickoutFromGroup(op.param1,[op.param2])
+		             if op.param2 in wait["blacklist"]:
+		                 pass
 		        else:
-		            random.choice(KAC).kickoutFromGroup(op.param1,[op.param2])
-                        if op.param2 in wait["blacklist"]:
-                            pass
-		        else:
-		            random.choice(DEF).inviteIntoGroup(op.param1,[op.param3])
+			    random.choice(KAC).inviteIntoGroup(op.param1,[op.param3])
 		    except:
 		        try:
-			    if op.param2 not in Bots:
-                                random.choice(DEF).kickoutFromGroup(op.param1,[op.param2])
+			    if op.param2 not in Creator:
+			        if op.param2 not in admin:
+			            if op.param2 not in Bots:
+                                        random.choice(KAC).kickoutFromGroup(op.param1,[op.param2])
 			    if op.param2 in wait["blacklist"]:
 			        pass
 			    else:
@@ -1214,25 +1210,25 @@ def bot(op):
                         if op.param2 in wait["blacklist"]:
                             pass
                         else:
-			    if op.param2 in Bots:
-			        pass
+			    if op.param2 in Creator:
+			        if op.param2 in admin:
+			            if op.param2 in Bots:
+			              pass
 			    else:
                                 wait["blacklist"][op.param2] = True
-           			f=codecs.open('st2__b.json','w','utf-8')
-              			json.dump(wait["blacklist"], f, sort_keys=True, indent=4,ensure_ascii=False)				
 		    if op.param2 in wait["blacklist"]:
                         pass
                     else:
-		        if op.param2 in Bots:
-			    pass
+		        if op.param2 in Creator:
+		            if op.param2 in admin:
+		                if op.param2 in Bots:
+			             pass
 		        else:
                             wait["blacklist"][op.param2] = True
-          		    f=codecs.open('st2__b.json','w','utf-8')
-           		    json.dump(wait["blacklist"], f, sort_keys=True, indent=4,ensure_ascii=False)
 		else:
 		    pass			
         if op.type == 19: #Member Ke Kick
-         if wait["AutoKick"] == True:
+         if wait["protect"] == True:
           if op.param2 in Bots:
             pass
           elif op.param2 in admin:
