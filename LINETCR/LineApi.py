006fb9ad908863dab00e4d49ed40a98ecdd6c5c8
raika44 <33775480+raika44@users.noreply.github.com>
2018-01-13 20:58:40 -0930

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -284,14 +284,8 @@ class LINE:
 
 
   def findAndAddContactsByMid(self, mid):
-        return self.Talk.client.findAndAddContactsByMid(0, mid)
+        return self.Talk.client.findAndAddContactsByMid(0, mid)  
 
-  def findAndAddContactByMid(self, mid):
-        return self.Talk.client.findAndAddContactByMid(0, mid)    
-
-  def findAndAddContactByMids(self, midlist):
-        for i in midlist:
-            self.Talk.client.findAndAddContactByMid(0, i)
 
   def findAndAddContactsByMids(self, midlist):
         for i in midlist:
@@ -301,14 +295,10 @@ class LINE:
   def findAndAddContactsByUserid(self, userid):
         return self.Talk.client.findAndAddContactsByUserid(0, userid)
 
-  def findAndAddContactByUserid(self, userid):
-        return self.Talk.client.findAndAddContactByUserid(0, userid)
     
   def findContactsByUserid(self, userid):
         return self.Talk.client.findContactsByUserid(userid)
 
-  def findContactByUserid(self, userid):
-        return self.Talk.client.findContactByUserid(userid)
     
   def findContactByTicket(self, ticketId):
         return self.Talk.client.findContactByUserTicket(ticketId)
