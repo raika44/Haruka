15c049fca48718813f90f1a8a0731459bfd2a0d7
raika44 <33775480+raika44@users.noreply.github.com>
2018-01-16 20:20:47 +0700

    Update iya.py

--- iya.py
+++ iya.py
@@ -2071,9 +2071,9 @@ def bot(op):
 			
             elif msg.text in ["Cl keluarallgrup"]:
               if msg.from_ in creator:
-				gid = cl.getGroupIdsJoined()				
-				for i in gid:	
-					cl.sendText(gid,"Dipaksa keluar oleh owner")					
+				gid = cl.getGroupIdsJoined()
+				cl.sendText(gid, "Dipaksa keluar oleh owner")				
+				for i in gid:					
 					cl.leaveGroup(i)
 	    elif "Ghost off" in msg.text:
               if msg.from_ in admin + staff + creator + peminjam:	 	        
