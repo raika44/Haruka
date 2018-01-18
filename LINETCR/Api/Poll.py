b670b0b8511188ef931ba77a50de7c3ac035afbd
raika44 <33775480+raika44@users.noreply.github.com>
2018-01-18 12:23:18 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Mozilla/5.0"
-  LA = "CHROMEOS\t1.7.18.0\tChrome_OS\t1"
+  UA = " Line/6.0.0 iPad4,1 9.0.2'"
+  LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
 
   rev = 0
 
