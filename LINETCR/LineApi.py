fd3321b192012edd83c2f457fd1d3ea610996e60
raika44 <33775480+raika44@users.noreply.github.com>
2018-01-18 10:13:16 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -42,7 +42,7 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'CHROMEOS\t1.7.18.0\tChrome_OS\t', 
+              'X-Line-Application': 'CHROMEOS\t1.7.18.0\tChrome_OS\t1', 
               'X-Line-Access': self.authToken, 
               'User-Agent': 'Mozilla/5.0'
    }
