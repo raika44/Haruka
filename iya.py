1f34ab56c6d92e0df39d2474a08a60cd912c1da0
raika44 <33775480+raika44@users.noreply.github.com>
2018-02-01 23:42:38 +0700

    Update iya.py

--- iya.py
+++ iya.py
@@ -6898,13 +6898,13 @@ def bot(op):
 #-----------------------------------------------
 #----------------------
             elif "Pap toket" in msg.text:
-              if msg.from_ in adminsa + penyewa:
+              if msg.from_ in adminsa + peminjam:
                 tanya = msg.text.replace("Pap toket","")
                 jawab = ("https://pbs.twimg.com/media/DQaSnbmUMAA67sE.jpg:large","https://4.bp.blogspot.com/-LUnpRcmusnk/WBtVDeVzYnI/AAAAAAAAAtA/PNMbmruQzYMBzOTFrRRhv2UVTGgnD6DmQCLcB/s400/IMG_20161102_212057.jpg","https://pbs.twimg.com/media/CzFralwUcAAuOuy.jpg","https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTilO50kExe4q_t-l8Kfn98sxyrHcbWPWCu2GP2SNgg8XWGMaZc8h5zaxAeVA","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKgSYYgB33GP3LAvVSYxKjDlbPokmtzSWjbWJogz8lbZMNSyvqJTE3qWpwBg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgwKO_CAdZpSlXVVfA29qglGQR00WHkeqq4JakyYDuzIW2tKhvGg","https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSC3ZMq4PnCX5dj7Fc_N6HOG6R_XrmOM7r6uBtpEcBfbO4hMEXQirK_lU_ePw","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgynJUxS4uYgaIiV_R6e4FY62QfhYRUEgYZg6psfJzWH_ci4dFng","https://2.bp.blogspot.com/-BBxz0kDierM/UX_0YauEVNI/AAAAAAAAFuc/vkUcWpqQOHU/s1600/ABG+Mansturbasi.jpg","https://pbs.twimg.com/media/BTRJOchCUAAlTqZ.jpg")
                 cl.sendImageWithURL(msg.to,jawaban)
 #-----------------------------------------------#----------------------
             elif "Pap anu" in msg.text:
-              if msg.from_ in adminsa + penyewa:
+              if msg.from_ in adminsa + peminjam:
                 tanya = msg.text.replace("Pap anu","")
                 jawab = ("https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQFFKdXErF56KzAa4oWnWQT34jmGKJ66lj1g0hnN4zwYh9GgW0dHWZfRnuM","https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQTn4_JMD1ZAg-XIk6JZ1Crhz9gtXEIS8AcjTA3SYmazAutt7ekHw","https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTIVuITo7KicaU6UwPhol1Rvkq4aQwznly8Xl2SiTlAa_1FrSHuwhwV5XoElA")
                 jawaban = random.choice(jawab)
